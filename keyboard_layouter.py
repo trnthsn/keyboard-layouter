0faa737b38b2d53b560892ac49407cf6f4491df0
Daraku-Neko <5214078+darakuneko@users.noreply.github.com>
2022-01-03 12:26:41 +0900

    Update keyboard_layouter.py

--- keyboard_layouter.py
+++ keyboard_layouter.py
@@ -62,7 +62,7 @@ class KeyboardLayouter(pcbnew.ActionPlugin):
         self.name = 'Keyboard Layouter Custom(added LEDs)'
         self.category = 'Modify PCB'
         self.description = 'Move parts to the position specified by json'
-        self.__version__ = '0.1.0'
+        self.__version__ = '0.1.2'
 
     def Run(self):
         self.__gui()
